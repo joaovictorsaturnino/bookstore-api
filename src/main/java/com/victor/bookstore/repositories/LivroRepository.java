03a55a4bd9848ea4cec85d70b36e8bb7ab1360d9
joaovictorsaturnino <victor.saturnino@esig.com.br>
2022-05-10 23:25:18 -0300

    Instância de forma automática da base de dados

--- src/main/java/com/victor/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/victor/bookstore/repositories/LivroRepository.java
@@ -3,9 +3,9 @@ package com.victor.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.victor.bookstore.domain.Categoria;
+import com.victor.bookstore.domain.Livro;
 
 @Repository
-public interface LivroRepositpry extends JpaRepository<Livro, Integer> {
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
 }
